d83e833b09ae4ae0a8109332dc333663bcd0d9a5
Batuhan Bilir <114738657+BlirBatuhan@users.noreply.github.com>
2023-12-12 16:16:34 +0300

    Extension
    uzantı eklenti ile kod okunabilirliği ve tekrarlarını önlemek

---
+++ Extansion/StringLib/StringLib.cs
@@ -0,0 +1,16 @@
+﻿namespace StringLib;
+
+public class StringLib
+{
+    string kelime = "Samsun Üniversitesi";
+    int boşluk;
+    int BoşlukBul()
+    {
+        foreach (var c in kelime)
+        {
+            if(c.Equals(' '))
+            boşluk += 1 ;
+        }
+        return boşluk;
+    }
+}
